d32d148167d27353e757e8623314d43059c63db5
kirkelifson <kirk@parodybit.net>
2013-03-14 15:10:33 -0400

    o-m-z installation hopefully works now

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh 
-chsh -s /bin/zsh 
+# install oh-my-zsh
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+
+# copy current dot files into home directory 
+# ^^ (way to do this dynamically?)
 cp -r .vim .vimrc .oh-my-zsh .zshrc .tmux.conf .gitconfig .gdbinit .htoprc update.sh ~
+
+# execute zshrc to see new config
 source ~/.zshrc
