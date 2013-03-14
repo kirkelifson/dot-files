51507ab4463ce378d8de79fdc0d41bc101637520
kirkelifson <kirk@parodybit.net>
2013-03-14 15:06:58 -0400

    added oh-my-zsh d/l and install into install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh 
+chsh -s /bin/zsh 
 cp -r .vim .vimrc .oh-my-zsh .zshrc .tmux.conf .gitconfig .gdbinit .htoprc update.sh ~
+source ~/.zshrc
