bbe7f801575e330e25c87cead460233177adb8a4
kirkelifson <kirk@parodybit.net>
2013-03-29 14:19:28 -0400

    fixed install script

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
-# install oh-my-zsh
-wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+#!/bin/zsh
+
+# install oh-my-zsh if not installed
+if [ ! -d ~/.oh-my-zsh ]; then
+    sudo apt-get install zsh
+    wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+fi
 
 # copy current dot files into home directory 
-# ^^ (way to do this dynamically?)
 cp -r .vim .vimrc .oh-my-zsh .zshrc .tmux.conf .gitconfig .gdbinit .htoprc update.sh ~
 
 # execute zshrc to see new config
