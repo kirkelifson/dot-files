9b701872bfd49124ce9e660718b59258a83788f1
kirkelifson <kirk@parodybit.net>
2013-07-11 13:09:30 -0400

    updated install script

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/zsh
 
+# assumed that zsh is installed
+
 # install oh-my-zsh if not installed
 if [ ! -d ~/.oh-my-zsh ]; then
-    sudo apt-get install zsh
     wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 fi
 
 # copy current dot files into home directory 
-cp -r .vim .vimrc .oh-my-zsh .zshrc .tmux.conf .gitconfig .gdbinit update.sh ~
+cp -r .vim .vimrc .zshrc .tmux.conf .gitconfig .gdbinit update.sh bench.sh ~
 
-# execute zshrc to see new config
+# execute zshrc to utilize new config
 source ~/.zshrc
