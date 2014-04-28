ea3d8a106e1cace4503bae005926adfdd3c8901c
kirkelifson <kirk@parodybit.net>
2014-04-28 11:49:40 -0400

    automatically remove orphaned and deinstalled packages

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,3 +3,6 @@ sudo apt-get upgrade
 sudo apt-get dist-upgrade
 sudo apt-get autoremove
 sudo apt-get autoclean
+
+dpkg --get-selections | grep deinstall | cut -f1 | xargs sudo apt-get -y purge
+deborphan | xargs sudo apt-get -y purge
