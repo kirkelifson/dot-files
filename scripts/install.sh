96afef16f9639a70e137fde8319998ab32b17ac8
kirkelifson <kirk@parodybit.net>
2014-05-24 17:47:50 -0400

    perfecting the installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,15 +3,22 @@
 # grab directory structure
 _dir="$(dirname $(readlink -f $0) | sed -e 's/\/scripts//')"
 
-# assumed: zsh installed and already being used
-
 # install oh-my-zsh if not installed
 if [ ! -d ~/.oh-my-zsh ]; then
     wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 fi
 
-# install using symlinks
+# Installation:
+
 # vim
+# backup ~/.vim
+if [ -d ~/.vim-backup ]; then
+    echo -ne "[!] Backing up ~/.vim to ~/.vim-backup\n\n"
+    rm -rf ~/.vim-backup
+fi
+
+mv ~/.vim ~/.vim-backup
+
 ln -sb $_dir/vim/.vimrc ~/
 cp -r $_dir/vim/.vim ~/
 
@@ -31,21 +38,16 @@ ln -sb $_dir/gdb/.gdbinit ~/
 # scripts
 ln -sb $_dir/scripts/update.sh ~/
 
+# delete all duplicated symlinks
+rm ~/*.*~
+rm ~/.*~
+
 # install vundle
 if [ ! -d ~/.vim/bundle/vundle ]; then
-    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle &> /dev/null
 fi
 
 vim +BundleInstall +qall
-
-# move backups into external folder
-if [ ! -d ~/.dotfile-backup ]; then
-    mkdir ~/.dotfile-backup
-fi
-
-mv ~/*.*~ ~/.dotfile-backup
-mv ~/.*~ ~/.dotfile-backup
-echo -n "All previous dot-files have been backed up into ~/.dotfile-backup\n"
 
 # execute zshrc to utilize new config
 source ~/.zshrc
