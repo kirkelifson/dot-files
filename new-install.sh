e1bbcd696d875a7469d996c2657e5258a75bf8b8
Kirk Elifson <kirk@parodybit.net>
2014-11-06 16:01:52 -0500

    added htop

--- new-install.sh
+++ new-install.sh
@@ -1,5 +1,5 @@
 cd ~
-sudo apt-get install build-essential curl gdb git libncurses5-dev linux-headers-$(uname -r) python2.7-dev ssh
+sudo apt-get install build-essential curl gdb git htop libncurses5-dev linux-headers-$(uname -r) python2.7-dev ssh
 sudo apt-get build-dep vim
 wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
 tar xzf vim-7.4.tar.bz2
