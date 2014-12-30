82b6d7da036d67d0174785be4c61adc84b662b51
Kirk Elifson <kirk@parodybit.net>
2014-12-30 14:32:20 -0500

    clean install scripts

--- new-install.sh
+++ new-install.sh
@@ -1,4 +1,4 @@
-cd ~
+cd
 sudo apt-get install build-essential curl gdb git htop libncurses5-dev linux-headers-$(uname -r) nasm python2.7-dev radare2 ssh tmux zsh
 sudo apt-get build-dep vim
 wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
@@ -6,7 +6,7 @@ tar xzf vim-7.4.tar.bz2
 cd vim74
 ./configure --enable-pythoninterp=yes --with-python-config-dir=/usr/lib/python2.7/config --enable-rubyinterp --with-features=huge
 make && sudo make install
-cd ~
+cd
 ssh-keygen
 git clone https://github.com/kirkelifson/dot-files
 cd dot-files
