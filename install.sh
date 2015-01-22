3c0e251bdb04e21895b63a78370c557a68ae6aa4
Kirk Elifson <kirk@parodybit.net>
2015-01-21 22:56:32 -0500

    eliminate unnecessary calls to pwd

--- install.sh
+++ install.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
 set -e
+dir=$(pwd)
 
 # Begin installation:
 echo -e "Installing dot-files:"
 echo -e "\t[+] Creating soft-links to dot-files."
 
 # Soft-link all files and directories (except .git of course)
-for dotfile in $(find $(pwd) -maxdepth 1 -name ".*" | sed '/.git$/d');
+for dotfile in $(find $dir -maxdepth 1 -name ".*" | sed '/.git$/d');
 do
-    echo -e "\t\t[>] Copying $dotfile"
     if [[ -d $dotfile ]];
     then
+        echo -e "\t\t[>] Copying $dotfile"
         cp -r $dotfile ~
     else
+        echo -e "\t\t[>] Linking $dotfile"
         ln -sf $dotfile ~
     fi
 done
 
 # Copy bin as well
-ln -sf $(pwd)/bin ~
+echo -e "\t\t[>] Linking bin"
+ln -sf $dir/bin ~
 
 # Only copy update script on debian
 if [[ `uname -s` == "Linux" ]];
 then
-    ln -sf `pwd`/update.sh ~
+    ln -sf $dir/update.sh ~
 fi
 
 # Install vundle
