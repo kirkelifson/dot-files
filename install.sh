8f95652b22388876307ba46c82fe31316f0d59d2
Kirk Elifson <kirk@parodybit.net>
2015-04-28 15:54:52 -0400

    echo and new lines are bad news

--- install.sh
+++ install.sh
@@ -4,22 +4,22 @@ set -e
 dir=$(pwd)
 
 # Begin installation:
-echo -ne "Installing dot-files:"
-echo -ne "\t[+] Creating soft-links to dot-files."
+echo -e "Installing dot-files:"
+echo -e "\t[+] Creating soft-links to dot-files."
 
 # Soft-link all files and directories (except .git of course)
 for dotfile in $(find $dir -maxdepth 1 -name ".*" | sed '/.git$/d'); do
     if [[ -d $dotfile ]]; then
-        echo -ne "\t\t[>] Copying $dotfile"
+        echo -e "\t\t[>] Copying $dotfile"
         cp -r $dotfile ~
     else
-        echo -ne "\t\t[>] Linking $dotfile"
+        echo -e "\t\t[>] Linking $dotfile"
         ln -sf $dotfile ~
     fi
 done
 
 # Copy bin as well
-echo -ne "\t\t[>] Linking $dir/bin"
+echo -e "\t\t[>] Linking $dir/bin"
 ln -sf $dir/bin ~
 
 # Only copy update script on debian
@@ -28,17 +28,17 @@ if [[ $(uname -s) == "Linux" ]]; then
 fi
 
 if [[ ! -d ~/peda ]]; then
-    echo -ne "\t[+] Installing peda (gdb)."
+    echo -e "\t[+] Installing peda (gdb)."
     git clone https://github.com/crowell/p3da ~/peda &> /dev/null
 fi
 
 # Install vundle
 if [[ ! -d ~/.vim/bundle/vundle ]]; then
-    echo -ne "\t[+] Installing vundle."
+    echo -e "\t[+] Installing vundle."
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle &> /dev/null
 fi
 
-echo -ne "\t[+] Installing new vundles."
+echo -e "\t[+] Installing new vundles."
 vim +PluginInstall +qall
 
-echo -ne "[!] Restart terminal session for changes to take effect."
+echo -e "[!] Restart terminal session for changes to take effect."
