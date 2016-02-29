518842dc2d66b164d6933f4a81d7993c259f5e78
Kirk Elifson <kirk@parodybit.net>
2016-02-29 14:24:10 -0500

    reindex install

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ echo -e "\t[+] Creating soft-links to dot-files."
 
 # Soft-link all files and directories (except .git of course)
 for dotfile in $(find $dir -maxdepth 1 -name ".*" | sed '/.git$/d'); do
-    if [[ -d $dotfile ]]; then
-        echo -e "\t\t[>] Copying $dotfile"
-        cp -r $dotfile ~
-    else
-        echo -e "\t\t[>] Linking $dotfile"
-        ln -sf $dotfile ~
-    fi
+  if [[ -d $dotfile ]]; then
+    echo -e "\t\t[>] Copying $dotfile"
+    cp -r $dotfile ~
+  else
+    echo -e "\t\t[>] Linking $dotfile"
+    ln -sf $dotfile ~
+  fi
 done
 
 # Copy bin as well
@@ -24,18 +24,18 @@ ln -sf $dir/bin ~
 
 # Only copy update script on debian
 if [[ $(uname -s) == "Linux" ]]; then
-    ln -sf $dir/update.sh ~
+  ln -sf $dir/update.sh ~
 fi
 
 if [[ ! -d ~/peda ]]; then
-    echo -e "\t[+] Installing peda (gdb)."
-    git clone https://github.com/longld/peda.git ~/peda &> /dev/null
+  echo -e "\t[+] Installing peda (gdb)."
+  git clone https://github.com/longld/peda.git ~/peda &> /dev/null
 fi
 
 # Install vundle
 if [[ ! -d ~/.vim/bundle/vundle ]]; then
-    echo -e "\t[+] Installing vundle."
-    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle &> /dev/null
+  echo -e "\t[+] Installing vundle."
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle &> /dev/null
 fi
 
 echo -e "\t[+] Installing new vundles."
