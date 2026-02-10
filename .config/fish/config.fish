1025e59fea54f2d719d63ee85cd1b354bf96aa94
Kirk Elifson <kirk@parodybit.net>
2026-02-10 08:29:55 -0500

    cleanup fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,15 +18,39 @@ set -gx DISABLE_SPRING 1
 set -gx FZF_DEFAULT_COMMAND 'fd --type f'
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 
-# Homebrew paths - always add (fish_add_path deduplicates)
-if test -d /opt/homebrew/bin
-    fish_add_path -p /opt/homebrew/bin /opt/homebrew/sbin
+# Disable greeting
+set -g fish_greeting
+
+# Detect platform once
+set -l os (uname -s)
+
+# --- PATH Management ---
+
+# Homebrew (macOS only)
+if test "$os" = Darwin
+    if test -d /opt/homebrew/bin
+        fish_add_path -p /opt/homebrew/bin /opt/homebrew/sbin
+
+        if test -d /opt/homebrew/opt/openjdk/bin
+            fish_add_path /opt/homebrew/opt/openjdk/bin
+        end
+    end
+
+    if test -d "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+        fish_add_path "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+    end
 end
+
+# Tool-specific paths (cross-platform)
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.bun/bin
+fish_add_path $HOME/.opencode/bin
+fish_add_path $HOME/.docker/bin
 
 # Only load other PATH modifications and integrations outside of tmux
 # (tmux inherits environment from parent shell)
 if not set -q TMUX
-    # Add ~/bin to PATH (fish_add_path automatically deduplicates)
     if test -d $HOME/bin
         fish_add_path -p $HOME/bin
     end
@@ -43,7 +67,9 @@ if not set -q TMUX
     end
 
     # LM Studio CLI
-    fish_add_path -a $HOME/.lmstudio/bin
+    if test -d $HOME/.lmstudio/bin
+        fish_add_path -a $HOME/.lmstudio/bin
+    end
 
     # Per-machine settings
     if test -f $HOME/.config/fish/local.fish
@@ -51,7 +77,22 @@ if not set -q TMUX
     end
 end
 
-# Aliases (loaded every time)
+# ASDF - only load if devenv is not active
+if not set -q DEVENV_PROFILE; and not set -q DEVENV_ROOT
+    if test -z $ASDF_DATA_DIR
+        set _asdf_shims "$HOME/.asdf/shims"
+    else
+        set _asdf_shims "$ASDF_DATA_DIR/shims"
+    end
+
+    if not contains $_asdf_shims $PATH
+        set -gx --prepend PATH $_asdf_shims
+    end
+    set --erase _asdf_shims
+end
+
+# --- Aliases ---
+
 alias be='bundle exec'
 alias clbin="curl -F 'clbin=<-' https://clbin.com"
 alias coverage-web="ruby -run -e httpd coverage -p 3005"
@@ -71,7 +112,7 @@ alias tmux='tmux -2 -u'
 alias jsc="env NODE_NO_READLINE=1 rlwrap node"
 
 # Platform-specific aliases
-switch (uname -s)
+switch $os
     case Linux
         alias apt='sudo apt'
         alias aptitude='sudo aptitude'
@@ -89,43 +130,3 @@ switch (uname -s)
         alias md5sum='md5 -q'
         alias ls='ls -Goh'
 end
-
-# Disable greeting
-set -g fish_greeting
-
-# opencode
-fish_add_path /Users/kronk/.opencode/bin
-
-fish_add_path /Users/kronk/.bun/bin
-
-fish_add_path /Users/kronk/.cargo/bin
-
-# Added by `rbenv init` on Mon Jan  5 14:06:01 EST 2026
-# status --is-interactive; and rbenv init - --no-rehash fish | source
-
-fish_add_path "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-
-fish_add_path /Users/kronk/.docker/bin
-
-# ASDF configuration code - only load if devenv is not active
-# Devenv sets its own environment and tool versions, so asdf should not interfere
-if not set -q DEVENV_PROFILE; and not set -q DEVENV_ROOT
-    if test -z $ASDF_DATA_DIR
-        set _asdf_shims "$HOME/.asdf/shims"
-    else
-        set _asdf_shims "$ASDF_DATA_DIR/shims"
-    end
-
-    # Do not use fish_add_path (added in Fish 3.2) because it
-    # potentially changes the order of items in PATH
-    if not contains $_asdf_shims $PATH
-        set -gx --prepend PATH $_asdf_shims
-    end
-    set --erase _asdf_shims
-end
-
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /Users/kronk/.lmstudio/bin
-# End of LM Studio CLI section
-
-fish_add_path /opt/homebrew/opt/openjdk/bin
